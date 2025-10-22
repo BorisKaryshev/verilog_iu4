8aa48c2b3f435cdb6d240a06dce6184772102f88
BorisKaryshev <boris.karyshev@gmail.com>
2025-10-22 18:23:57 +0000

    Fixed sem2_task2 testing with gtest

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 set -x
 
 base_path="$(pwd)"
-for path in $(echo sem*); do
+for path in sem1 sem2 sem3 sem4 sem5; do
     cd $base_path/$path
     cmake -B build -S .
     cmake --build build
